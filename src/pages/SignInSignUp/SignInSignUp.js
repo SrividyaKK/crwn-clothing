dd790edd4acc7fb9b0e6b70bbe0106375ec1e251
SrividyaKK <SrividyaKK@users.noreply.github.com>
2019-07-20 22:15:42 +0530

    Added sign in components

---
+++ src/pages/SignInSignUp/SignInSignUp.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import SignIn from '../../components/SignIn/SignIn';
+
+import './SignInSignUp.scss';
+
+const SignInSignUpPage = () => {
+    return (
+        <div>
+            <SignIn />
+        </div>
+    );
+}
+
+export default SignInSignUpPage;
