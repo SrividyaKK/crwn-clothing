8bcb3ec92718eb26a83acc431bb07323982a864e
SrividyaKK <SrividyaKK@users.noreply.github.com>
2019-07-23 15:05:54 +0530

    added cart items in cart dropdown

--- src/components/CartDropdown/CartDropdown.js
+++ src/components/CartDropdown/CartDropdown.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import CustomButton from '../CustomButton/CustomButton';
 
 import './CartDropdown.scss';
+import CartItem from '../CartItem/CartItem';
+import { isTemplateElement } from '@babel/types';
 
-const CartDropdown = () => (
+const CartDropdown = ({ cartItems }) => (
     <div className='cart-dropdown'>
-        <div className='cart-items' />
+        <div className='cart-items'>
+            {
+                cartItems.map(item => <CartItem key={isTemplateElement.id} item={item} />)
+            }
+        </div>
         <CustomButton>GO TO CHECKOUT</CustomButton>
     </div>
 );
 
-export default CartDropdown;
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+    cartItems,
+});
+
+export default connect(mapStateToProps)(CartDropdown);
--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -5,3 +5,11 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
     }
     return [...cartItems, { ...cartItemToAdd, quantity: 1 }]; // the item is not present in the cart
 }
+
+export const removeItemFroCart = (cartItems, cartItemToRemove) => {
+    const existingCartItem = cartItems.find(x => x.id === cartItemToRemove.id);
+    if (existingCartItem) {
+        return cartItems.map(cartItem => cartItem.id === cartItemToRemove.id ? { ...addItemToCart.cartItem, quantity: cartItem.quantity - 1 } : cartItem);
+    }
+    // return [...cartItems, { ...cartItemToRemove, quantity: 1 }]; // the item is not present in the cart
+}
